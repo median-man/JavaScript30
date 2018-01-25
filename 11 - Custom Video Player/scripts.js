2eb3cb0144fecbbb4ee7b6c513b8ff04102a9bdd
John Desrosiers <johndrunner@yahoo.com>
2018-01-25 06:33:14 -0800

    Add scrub to exercise 11 solution

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,7 +3,8 @@ const controls = document.getElementsByClassName('player__controls')[0];
 const sliderVolume = document.getElementsByName('volume')[0];
 const sliderRate = document.getElementsByName('playbackRate')[0];
 const progressBar = document.getElementsByClassName('progress__filled')[0];
-
+const progressBarContainer = document.querySelector('.progress');
+let scrubFlag = false;
 
 function togglePlay() {
   const btnToggle = document.getElementsByClassName('toggle')[0];
@@ -31,9 +32,16 @@ function skip(seconds) {
 }
 
 // update position of video progress bar
-function updateProgress() {
+function updateProgressBar() {
   const progress = video.currentTime / video.duration || 0;
   progressBar.style['flex-basis'] = `${progress * 100}%`;
+}
+
+function changeProgress(event) {
+  if (scrubFlag || event.type === 'click') {
+    const { x, width } = this.getBoundingClientRect();
+    video.currentTime = ((event.clientX - x) / width) * video.duration;
+  }
 }
 
 function controlClick(event) {
@@ -43,10 +51,14 @@ function controlClick(event) {
 }
 
 // set progress bar to match current video playback time
-updateProgress();
+updateProgressBar();
 
 video.addEventListener('click', togglePlay);
-video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('timeupdate', updateProgressBar);
 sliderVolume.addEventListener('change', changeVolume);
 sliderRate.addEventListener('change', changeRate);
 controls.addEventListener('click', controlClick);
+progressBarContainer.addEventListener('click', changeProgress);
+progressBarContainer.addEventListener('mousemove', changeProgress);
+progressBarContainer.addEventListener('mousedown', () => { scrubFlag = true; });
+document.addEventListener('mouseup', () => { scrubFlag = false; });
